e8a5db99b6ed288cb101032ae365f47caabbdc23
Aditya Gupta <adityag1998@gmail.com>
2020-10-07 16:47:42 +0530

    Tried implementing broadcast intent fire
    1. The code to actual logic is commented in this commit
    2. Need to resolve concurrentThreadException
    3. Cause of Occurence:
    Deletion in objectDict in objectDictMaintainer
    4. Solution
    Use iterator as shown in https://www.java67.com/2017/06/how-to-remove-entry-keyvalue-from-HashMap-in-java.html

--- app/src/main/java/com/samsung/navicam/MainActivity.kt
+++ app/src/main/java/com/samsung/navicam/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.samsung.navicam
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -19,65 +20,83 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    // Static variables
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        lateinit var appContext:Context
+    }
 
+    // Class members
     private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
     private lateinit var camera: Camera
     private lateinit var preview: Preview
     private lateinit var imageCapture: ImageCapture
     private lateinit var cameraSelector: CameraSelector
     private lateinit var classificationExecutor: ExecutorService
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Request camera permissions
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-            )
-        }
-
-        // Set up the listener for take photo button
-        camera_capture_button.setOnClickListener { takePhoto() }
-
-        // Fetch output Directory
-        outputDirectory = getOutputDirectory()
-
-        // Fetch cameraExecutor
-        cameraExecutor = Executors.newSingleThreadExecutor()
-        classificationExecutor = Executors.newSingleThreadExecutor()
+    // Class helper functions
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            }
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            //Will be used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            // ImageCapture
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            // Log.d(TAG, "startCamera before objectAnalyzer: ${Thread.currentThread().name}")
+            // Bind ObjectAnalyzer on a separate background thread classificationExecutor
+            val objectAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also { it.backpressureStrategy }
+                .also { it.setAnalyzer(classificationExecutor, ObjectAnalyzer())
+                }
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, objectAnalyzer
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun takePhoto() {
-        Log.d(TAG, "takePhoto: I heard you Button Click")
-
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-        Log.d(TAG, "takePhoto: Got Stable Image Capture")
-
         // Create time-stamped output file to hold the image
         val photoFile = File(
             outputDirectory,
@@ -86,11 +105,9 @@ class MainActivity : AppCompatActivity() {
             ).format(System.currentTimeMillis()) + ".jpg"
         )
         Log.d(TAG, "takePhoto: File Created Successfully")
-
         // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
         Log.d(TAG, "takePhoto: Object = (File + metadata) created successfully")
-
         // Set up image capture listener, which is triggered after photo has
         // been taken
         imageCapture.takePicture(
@@ -110,81 +127,53 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        cameraProviderFuture.addListener(Runnable {
-            //Will be used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+        // Set Context
+        appContext = applicationContext
 
-            // Preview
-            preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewFinder.surfaceProvider)
-                }
-
-            //ImageCapture
-            imageCapture = ImageCapture.Builder().build()
-
-            // Select back camera as a default
-            cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-//            // Bind ImageAnalyzer
-//            val imageAnalyzer = ImageAnalysis.Builder()
-//                .build()
-//                .also {
-//                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-//                        Log.d(TAG, "Average luminosity: $luma")
-//                    })
-//                }
-
-            // Bind ObjectAnalyzer
-            val objectAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also { it.backpressureStrategy }
-                .also { it.setAnalyzer(classificationExecutor, ObjectAnalyzer()) }
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, objectAnalyzer
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
+
+        // Set up the listener for take photo button
+        camera_capture_button.setOnClickListener { takePhoto() }
+
+        // Fetch output Directory
+        outputDirectory = getOutputDirectory()
+
+        // Spawn background thread
+        classificationExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cameraExecutor.shutdown()
         classificationExecutor.shutdown()
-    }
-
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
--- app/src/main/java/com/samsung/navicam/ObjectAnalyzer.kt
+++ app/src/main/java/com/samsung/navicam/ObjectAnalyzer.kt
@@ -1,10 +1,14 @@
 package com.samsung.navicam
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabel
 import com.google.mlkit.vision.label.ImageLabeling
@@ -14,80 +18,208 @@ import com.google.mlkit.vision.text.TextRecognition
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.ArrayList
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 class ObjectAnalyzer : ImageAnalysis.Analyzer {
 
-    private suspend fun startImageClassification(
+    companion object {
+        //Static Members
+        const val CONFIDENCE_THRESHOLD = 0.7f
+        const val TAG = " ObjectAnalyzer"
+        const val BENEFICIARY = "com.samsung.smartnotes"
+        const val PARSE_BUNDLE = "com.samsung.navicam.parse_bundle"
+        const val KEY1 = "com.samsung.navicam.objectList"
+        const val KEY2 = "com.samsung.navicam.text"
+        const val NOISE_CANCELLATION_BUFFER = 10
+        var text: Text? = null
+        var objectDict: HashMap<String,Int> = HashMap()
+        var prevObjectSet = HashSet<String>()
+
+        // Static helper function
+        fun isPackageInstalled(packageName: String, pm: PackageManager): Boolean {
+            return try {
+                pm.getApplicationInfo(packageName, 0).enabled
+                true
+            }
+            catch (e: PackageManager.NameNotFoundException){
+                false
+            }
+        }
+
+        // Constructor of static block
+        init {
+            Log.d(TAG, "initialize objectDict class: ${Thread.currentThread().name}")
+        }
+    }
+
+    private fun displayObjectLabels(labels: List<ImageLabel>){
+        for (label in labels){
+            val text = label.text
+            val confidence = label.confidence
+            val index = label.index
+            Log.d(TAG, "displayObjectLabels: Object${index}, Confidence: $confidence, $text")
+        }
+    }
+
+    private fun showFireToast(objectList: ArrayList<String>, text: String, context: Context){
+        val toastText:String = """
+            Broadcast is Fired: 
+            Object List Size: ${objectList.size}
+            Text String Length: ${text.length}
+            """.trimIndent()
+
+        Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showFailToast(context: Context){
+        Toast.makeText(context,
+            "Companion App: com.samsung.smartnotes Not Found",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    private fun sendBroadcastIntent(bundle: Bundle, context: Context){
+        Intent().also { intent ->
+            intent.action = PARSE_BUNDLE
+            intent.addCategory("android.intent.category.DEFAULT")
+            intent.setPackage(BENEFICIARY)
+            intent.putExtras(bundle)
+            context.sendBroadcast(intent)
+        }
+    }
+
+    private fun getObjectList(currObjectSet: java.util.HashSet<String>): ArrayList<String> {
+        return ArrayList(currObjectSet)
+    }
+
+    private fun getText(visionText: Text?): String{
+        return visionText?.text ?: ""
+    }
+
+    private fun getBundle(objectList: ArrayList<String>, text: String): Bundle{
+        val bundle = Bundle()
+        bundle.putSerializable(KEY1, objectList)
+        bundle.putSerializable(KEY2, text)
+        return bundle
+    }
+
+    private fun processBroadcast(currObjectSet: java.util.HashSet<String>){
+        // Get Context
+        val context:Context = MainActivity.appContext
+
+        if (isPackageInstalled(BENEFICIARY, context.packageManager)){
+            val objectList = getObjectList(currObjectSet)
+            val text = getText(text)
+            val bundle = getBundle(objectList, text)
+            sendBroadcastIntent(bundle, context)
+            showFireToast(objectList, text, context)
+        }
+
+        else{
+            showFailToast(context)
+        }
+
+    }
+
+    private fun getSharableObjectSet(objectDict: HashMap<String, Int>) {
+        var currObjectSet = java.util.HashSet<String>()
+        for (key in objectDict.keys){
+            if (objectDict[key] == NOISE_CANCELLATION_BUFFER){
+                currObjectSet.add(key)
+            }
+        }
+
+        if (!currObjectSet.equals(prevObjectSet)){
+            processBroadcast(currObjectSet)
+            prevObjectSet = currObjectSet
+        }
+    }
+
+    // Logic to maintain a dictionary in Static Variable
+    private fun objectDictMaintainer(labels: List<ImageLabel>) {
+        for (key in objectDict.keys){
+            // Decrement all Objects by 1
+            objectDict[key] = objectDict[key]!! - 1
+
+            // If Object Freq is 0 remove it from Dict
+            if (objectDict[key] == 0){
+                objectDict.remove(key)
+            }
+        }
+
+        // Increment found objects by 2
+        for (label in labels){
+            val key = label.text
+            if (objectDict.containsKey(key) && objectDict[key]!! <= NOISE_CANCELLATION_BUFFER-2){
+                objectDict[key] = objectDict[key]!! + 2
+            }
+            else if (objectDict.containsKey(key) && objectDict[key]!! > NOISE_CANCELLATION_BUFFER-2){
+                objectDict[key] = NOISE_CANCELLATION_BUFFER
+            }
+            else{
+                objectDict[key] = 1
+            }
+        }
+
+        // Net Result Old objects removed, new objects introduced and Updated Dict
+        // Call getSharableObjectSet
+        getSharableObjectSet(objectDict)
+    }
+
+    private fun startImageClassification(
         processableImage: InputImage,
         image: ImageProxy,
-        textReaderJob: Job,
+        textReadJob: Job,
     ): Unit {
         //Set Options and get an Image Labeler
         val options = ImageLabelerOptions.Builder()
-            .setConfidenceThreshold(0.7f)
+            .setConfidenceThreshold(CONFIDENCE_THRESHOLD)
             .build()
         val labeler = ImageLabeling.getClient(options)
 
         // Pass processableImage to Labeler
-
          labeler.process(processableImage)
             .addOnSuccessListener { labels ->
-                for (label in labels) {
-                    val text = label.text
-                    val confidence = label.confidence
-                    val index = label.index
-                    Log.d(TAG, "Object:$index $text Confidence:$confidence")
-                }
+                displayObjectLabels(labels)
+                //objectDictMaintainer(labels)
             }
             .addOnFailureListener { exc ->
                 Log.e(TAG, "ObjectLabeler: $exc")
             }
             .addOnCompleteListener { _ ->
-                textReaderJob.invokeOnCompletion {
+                textReadJob.invokeOnCompletion {
                     image.close()
                 }
             }
     }
 
-    private suspend fun startTextClassification(
+    private fun startTextClassification(
         processableImage: InputImage,
-        image: ImageProxy
     ): Unit {
+
         val recognizer = TextRecognition.getClient()
 
         recognizer.process(processableImage)
             .addOnSuccessListener { visionText ->
                 Log.d(TAG, "Text: ${visionText.text}")
+                //text = visionText
             }
             .addOnFailureListener { exc ->
                 Log.e(TAG, "TextIdentifier: $exc")
             }
     }
 
-    @SuppressLint("UnsafeExperimentalUsageError")
-    override fun analyze(image: ImageProxy) = runBlocking {
-        val intermediate = image.image
 
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(image: ImageProxy) = runBlocking{
+        val intermediate = image.image
+        Log.d(TAG, "analyze override fn: ${Thread.currentThread().name}")
         if (intermediate != null) {
             val processableImage =
                 InputImage.fromMediaImage(intermediate, image.imageInfo.rotationDegrees)
-
-            Log.d(TAG, "beforeCoroutineCreation: ${Thread.currentThread().name}")
-            val startTime = System.currentTimeMillis()
-            val textReaderJob = launch {
-                Log.d(TAG, "Job2: TextClassification ${Thread.currentThread().name}")
-                startTextClassification(processableImage, image)
-            }
-
-            val imageClassifierJob = launch {
-                Log.d(TAG, "Job1: ImageClassification ${Thread.currentThread().name}")
-                startImageClassification(processableImage, image, textReaderJob)
-            }
-
+            val textReadJob = launch { startTextClassification(processableImage) }
+            launch {startImageClassification(processableImage, image, textReadJob)}
         }
-    }
-
-    companion object {
-        const val TAG = " ObjectAnalyzer"
     }
 }
