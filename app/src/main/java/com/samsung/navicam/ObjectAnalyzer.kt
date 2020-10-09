f107342b14c3fb023682957836d9842f0049c494
Aditya Gupta <adityag1998@gmail.com>
2020-10-09 18:42:18 +0530

    Replaced text equality with Levenshtein Distance
    1. ML Frame prediction of text recognition isn't perfect.
    2. We thus need to replace Text Equality with Text Similarity
    3. Cosine Similarity and Levenshtein Distance are two most prominent techniques for same.
    4. Implemented using LEVENSHTEIN_DISTANCE_THRESHOLD, static value.

--- app/src/main/java/com/samsung/navicam/ObjectAnalyzer.kt
+++ app/src/main/java/com/samsung/navicam/ObjectAnalyzer.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
+import kotlin.math.min
 
 class ObjectAnalyzer : ImageAnalysis.Analyzer {
 
@@ -34,6 +35,7 @@ class ObjectAnalyzer : ImageAnalysis.Analyzer {
         const val KEY2 = "com.samsung.navicam.blockWiseTextList"
         const val NOISE_CANCELLATION_BUFFER = 20
         const val REMOVE_OBJECT_NOISE_THRESHOLD = 10
+        const val LEVENSHTEIN_DISTANCE_THRESHOLD = 10
         var visionTextObject: Text? = null
         var objectDict: HashMap<String, Int> = HashMap()
         var prevObjectSet = HashSet<String>()
@@ -102,6 +104,34 @@ class ObjectAnalyzer : ImageAnalysis.Analyzer {
         Log.d(TAG, "displayBlockWiseText: --Text Blocks End--")
     }
 
+    private fun levenshtein(lhs : CharSequence, rhs : CharSequence) : Int {
+        val lhsLength = lhs.length
+        val rhsLength = rhs.length
+
+        var cost = Array(lhsLength) { it }
+        var newCost = Array(lhsLength) { 0 }
+
+        for (i in 1 until rhsLength) {
+            newCost[0] = i
+
+            for (j in 1 until lhsLength) {
+                val match = if(lhs[j - 1] == rhs[i - 1]) 0 else 1
+
+                val costReplace = cost[j - 1] + match
+                val costInsert = cost[j] + 1
+                val costDelete = newCost[j - 1] + 1
+
+                newCost[j] = min(min(costInsert, costDelete), costReplace)
+            }
+
+            val swap = cost
+            cost = newCost
+            newCost = swap
+        }
+
+        return cost[lhsLength - 1]
+    }
+
     private fun sendBroadcastIntent(bundle: Bundle, context: Context){
         Intent().also { intent ->
             intent.action = PARSE_BUNDLE
@@ -127,7 +157,10 @@ class ObjectAnalyzer : ImageAnalysis.Analyzer {
     }
 
     private fun setVisionTextObject(visionText: Text?){
-        if (visionText != visionTextObject){
+        val prevText: CharSequence = visionTextObject?.text.toString()
+        val currText:CharSequence = visionText?.text.toString()
+        val levenshteinDistance = levenshtein(prevText, currText)
+        if (levenshteinDistance >= LEVENSHTEIN_DISTANCE_THRESHOLD){
             visionTextObject = visionText
             processBroadcastText()
         }
